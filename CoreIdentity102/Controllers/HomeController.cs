52733eb1e776f05f60925b6a7d5fd0f06322b958
Hüseyin Yıldız <huseyinyildiz005@gmail.com>
2022-05-11 15:43:10 +0300

    Add project files.

---
+++ CoreIdentity102/Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using CoreIdentity102.Models;
+using CoreIdentity102.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreIdentity102.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userName = await _userManager.FindByNameAsync(model.UserName);
+                var pass = await _userManager.CheckPasswordAsync(userName, model.Password);
+                if (pass != false)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(String.Empty, "Invalid UserName or Password");
+                }
+
+            }
+            return View(model);//modeli döndürüyorum ki kullanıcı yaptığı hatayı görebilsin 
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser();
+                user.UserName = model.UserName;
+                user.Email = model.Email;
+                user.PhoneNumber = model.PhoneNumber;
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code,item.Description);
+                    }
+                }
+            }
+            return View(model);//modeli döndürüyorum ki kullanıcı yaptığı hatayı görebilsin 
+        }
+    
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
